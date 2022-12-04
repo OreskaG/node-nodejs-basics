e0ab9bfe25947cf514ec0f47c4132c5ce41e09c1
OreskaG <Ctepanrfo@gmail.com>
2022-12-04 23:23:03 +0300

    refactor: fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,11 +2,11 @@ import { writeFile } from "fs/promises"
 import { dirPath, ERROR_MSG } from "../utils.js";
 
 const create = async () => {
-    const folder = `${(await dirPath(import.meta.url))}/files/fresh.txt`;
+    const filePath = `${(await dirPath(import.meta.url))}/files/fresh.txt`;
     const text = 'I am fresh and young';
 
     try {
-        await writeFile(folder, text, { flag: 'wx'});
+        await writeFile(filePath, text, { flag: 'wx'});
     } catch (err) {
         throw new Error(ERROR_MSG);
     }
