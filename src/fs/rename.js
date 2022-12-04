c606d03c7fbea605a0c590c2a530e38489c4f39a
OreskaG <Ctepanrfo@gmail.com>
2022-12-04 23:25:18 +0300

    refactor: fs rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -2,11 +2,11 @@ import { rename as _rename } from "fs/promises"
 import { dirPath, ERROR_MSG } from "../utils.js";
 
 const rename = async () => {
-    const file = `${(await dirPath(import.meta.url))}/files/properFilename.md`;
-    const fileWrong = `${(await dirPath(import.meta.url))}/files/wrongFilename.txt`;
+    const filePath = `${(await dirPath(import.meta.url))}/files/properFilename.md`;
+    const wrongFilePath = `${(await dirPath(import.meta.url))}/files/wrongFilename.txt`;
 
     try {
-        await _rename(fileWrong, file);
+        await _rename(wrongFilePath, filePath);
     } catch (err) {
         throw new Error(ERROR_MSG);
     }
