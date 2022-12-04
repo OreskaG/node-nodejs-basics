4138b8b35776dc3bd2ce96fa9cd7e727eb009c5e
OreskaG <Ctepanrfo@gmail.com>
2022-12-04 23:41:13 +0300

    feat: fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from "fs/promises"
+import { dirPath, ERROR_MSG } from "../utils.js";
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = `${(await dirPath(import.meta.url))}/files`;
+
+    try {
+        const filesInFolder = await readdir(folderPath);
+        console.log(filesInFolder);
+    } catch (err) {
+        throw new Error(ERROR_MSG);
+    }
 };
 
 await list();
