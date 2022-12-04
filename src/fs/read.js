999de35a3e160819607c9e175203c08a079649ec
OreskaG <Ctepanrfo@gmail.com>
2022-12-04 23:49:29 +0300

    feat: fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { readFile } from "fs/promises"
+import { dirPath, ERROR_MSG } from "../utils.js";
+
 const read = async () => {
-    // Write your code here 
+    const filePath = `${(await dirPath(import.meta.url))}/files/fileToRead.txt`;
+
+    try {
+        const text = await readFile(filePath, 'utf-8');
+        console.log(text);
+    } catch (err) {
+        throw new Error(ERROR_MSG);
+    }
 };
 
 await read();
