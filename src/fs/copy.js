bfaf4694c830bff1d051db783353acdd373914eb
OreskaG <Ctepanrfo@gmail.com>
2022-12-04 23:23:46 +0300

    refactor: fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,11 +2,11 @@ import { cp } from "fs/promises"
 import { dirPath, ERROR_MSG } from "../utils.js";
 
 const copy = async () => {
-    const folder = `${(await dirPath(import.meta.url))}/files`;
-    const folderCopy = `${(await dirPath(import.meta.url))}/files_copy`;
+    const folderPath = `${(await dirPath(import.meta.url))}/files`;
+    const folderCopyPath = `${(await dirPath(import.meta.url))}/files_copy`;
 
     try {
-        await cp(folder, folderCopy, {recursive: true, force: false, errorOnExist: true})
+        await cp(folderPath, folderCopyPath, {recursive: true, force: false, errorOnExist: true})
     } catch (err) {
         throw new Error(ERROR_MSG);
     }
