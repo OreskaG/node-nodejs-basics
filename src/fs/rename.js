bb696d07016a6595442c10c67c4d2e8e87f1ea69
OreskaG <Ctepanrfo@gmail.com>
2022-12-04 23:04:34 +0300

    feat: fs rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import { rename as _rename } from "fs/promises"
+import { dirPath, ERROR_MSG } from "../utils.js";
+
 const rename = async () => {
-    // Write your code here 
+    const file = `${(await dirPath(import.meta.url))}/files/properFilename.md`;
+    const fileWrong = `${(await dirPath(import.meta.url))}/files/wrongFilename.txt`;
+
+    try {
+        await _rename(fileWrong, file);
+    } catch (err) {
+        throw new Error(ERROR_MSG);
+    } 
 };
 
 await rename();
