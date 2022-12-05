23ba3dc41954c4b20127f35315f018a54cfe4ed7
OreskaG <Ctepanrfo@gmail.com>
2022-12-05 20:45:39 +0300

    feat: zip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import { createWriteStream, createReadStream } from "fs"
+import { createGunzip } from "zlib";
+import { dirPath } from "../utils.js";
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(`${(await dirPath(import.meta.url))}/files/archive.gz`);
+    const writeStream = createWriteStream(`${(await dirPath(import.meta.url))}/files/fileToCompress.txt`);
+    const unzip = createGunzip();
+
+    readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
