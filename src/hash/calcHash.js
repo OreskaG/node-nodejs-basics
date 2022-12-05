c34aa94cbfd4bda40dbe60225be7cd7d077ad3b3
OreskaG <Ctepanrfo@gmail.com>
2022-12-05 18:41:34 +0300

    feat: hash calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import { createHash } from "crypto"
+import { readFile } from "fs/promises";
+import { dirPath } from "../utils.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const text = await readFile(`${(await dirPath(import.meta.url))}/files/fileToCalculateHashFor.txt`);
+    console.log(createHash('sha256').update(text).digest('hex'));
 };
 
 await calculateHash();
