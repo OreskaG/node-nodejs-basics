d445907a07ba4f120fc7938c6650c700174f15a4
OreskaG <Ctepanrfo@gmail.com>
2022-12-05 22:31:13 +0300

    feat: wt worker and main

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { dirPath } from "../utils.js";
+
 const performCalculations = async () => {
-    // Write your code here
+    let count = 10;
+
+    const result = await Promise.allSettled(cpus().map(() => {
+        return new Promise(async (res, rej) => {
+            const worker = new Worker(`${(await dirPath(import.meta.url))}/worker.js`, { workerData: count++ });
+            worker.on('message', res);
+            worker.on('error', rej);
+        })
+    }));
+
+    result.map(((data) => {
+        data.status === 'fulfilled' ? data.status = 'resolved' : data.status = 'error';
+        if (data.status === 'error') {
+            data.value = null;
+            delete data.reason;
+        };
+    }))
+
+    console.log(result);
 };
 
 await performCalculations();
